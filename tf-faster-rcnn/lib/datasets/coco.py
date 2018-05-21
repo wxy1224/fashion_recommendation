e59d043ff99dd9ba6fb260e5a6188066a14eb5f3
Fei Liu <liufei11111@hotmail.com>
2018-05-21 02:33:39 -0700

    alexnet supported

--- tf-faster-rcnn/lib/datasets/coco.py
+++ tf-faster-rcnn/lib/datasets/coco.py
@@ -67,7 +67,7 @@ class coco(imdb):
     prefix = 'instances' if self._image_set.find('test') == -1 \
       else 'image_info'
     return osp.join(self._data_path, 'annotations',
-                    coco + '_' + self._image_set + self._year + '.json')
+                    'instances_' + self._image_set + self._year + '.json')
 
   def _load_image_set_index(self):
     """
