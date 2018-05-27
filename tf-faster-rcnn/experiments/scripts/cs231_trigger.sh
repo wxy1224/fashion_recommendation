67e65790ac1a0ba3327b910f9f362b875d7f4521
FeiLiu <liufei11111@hotmail.com>
2018-05-26 21:29:13 -0700

    no need for second test

--- tf-faster-rcnn/experiments/scripts/cs231_trigger.sh
+++ tf-faster-rcnn/experiments/scripts/cs231_trigger.sh
@@ -3,5 +3,4 @@ python ../deep_fashion_to_coco.py
 rm -rf data/cache
 rm -rf output/vgg16
 ./experiments/scripts/train_faster_rcnn.sh 0 coco vgg16
-./experiments/scripts/test_faster_rcnn.sh 0 coco vgg16
 mv output/vgg16 output/vgg16_prev
