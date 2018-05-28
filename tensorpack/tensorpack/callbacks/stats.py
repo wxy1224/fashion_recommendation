2ca3a7e0850da701fc8e3421a06e00531b7286ba
wxy1224 <xw288@cornell.edu>
2018-05-27 20:05:01 -0700

    Revert "add tensorpack as a whole"
    This reverts commit c9cbf44e724f52e7e5fae6d72aca26bd781c6e9f.

--- tensorpack/tensorpack/callbacks/stats.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# File: stats.py
-
-# for compatibility only
-from .misc import InjectShell, SendStat     # noqa
-from .graph import DumpParamAsImage     # noqa
-
-__all__ = []
--- tensorpack/tensorpack/train/utility.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# File: utility.py
-
-# for backwards-compatibility
-from ..graph_builder.utils import (  # noqa
-    OverrideToLocalVariable,
-    override_to_local_variable, LeastLoadedDeviceSetter)
